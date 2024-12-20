f5d7cd612821ae58a794ebe285d59c5df576384d
admiral <kartikeyk1776@gmail.com>
2024-12-20 15:25:34 +0530

    minor ui changes

--- lib/news_app.dart
+++ lib/news_app.dart
@@ -4,6 +4,7 @@ import 'package:api_project/my_news.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsApp extends StatefulWidget {
   const NewsApp({super.key});
@@ -37,15 +38,21 @@ class NewsAppPage extends State<NewsApp> {
       body: FutureBuilder(
           future: fetchNews(),
           builder: (context, snapsot) {
-            final article = snapsot.data!.articles!;
+            final article = snapsot.data?.articles!;
 
             if (snapsot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (!snapsot.hasData) {
               return const Center(
                 child: CircularProgressIndicator(),
               );
             } else {
               return ListView.builder(
                 itemBuilder: (context, index) {
+                  final newsSource = Uri.parse(article?[index].url ?? '');
+
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Container(
@@ -62,26 +69,32 @@ class NewsAppPage extends State<NewsApp> {
                           ListTile(
                             leading: CircleAvatar(
                               backgroundImage: NetworkImage(
-                                  article[index].urlToImage ?? 'no data'),
+                                  article?[index].urlToImage ?? 'no data'),
                             ),
-                            title: Text(article[index].title ?? 'no data'),
-                            subtitle: Text(article[index].author ?? 'no data'),
+                            title: Text(article?[index].author ?? 'no data'),
                             trailing: Text(
-                              article[index].publishedAt ?? 'no data',
+                              article?[index].publishedAt ?? 'no data',
                             ),
                           ),
                           Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              article[index].content ?? 'no data',
-                            ),
-                          )
+                            child: Text(article?[index].title ?? 'no data'),
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextButton(
+                                onPressed: () => launchUrl(newsSource),
+                                child: const Text(
+                                  'source',
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                              )),
                         ],
                       ),
                     ),
                   );
                 },
-                itemCount: article.length,
+                itemCount: article?.length,
               );
             }
           }),
