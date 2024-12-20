8b2be487aae111b2030dc4064dbff06c66fd9023
admiral <kartikeyk1776@gmail.com>
2024-12-21 00:27:10 +0530

    modified ui

--- lib/news_app.dart
+++ lib/news_app.dart
@@ -4,6 +4,7 @@ import 'package:api_project/my_news.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class NewsApp extends StatefulWidget {
@@ -52,33 +53,72 @@ class NewsAppPage extends State<NewsApp> {
               return ListView.builder(
                 itemBuilder: (context, index) {
                   final newsSource = Uri.parse(article?[index].url ?? '');
-
+                  DateTime time =
+                      DateTime.parse(article?[index].publishedAt ?? '');
+                  String finalTime =
+                      DateFormat('HH:mm, MMM d, yyyy').format(time);
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Container(
-                      decoration: const BoxDecoration(
-                          gradient: LinearGradient(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12.0),
+                          gradient: const LinearGradient(
                               begin: Alignment.topLeft,
                               end: Alignment.bottomLeft,
                               colors: [
-                            Color.fromARGB(255, 51, 204, 255),
-                            Color.fromARGB(255, 255, 255, 0),
-                          ])),
+                                Color(0xff3366cc),
+                                Color(0xffffff99),
+                              ])),
                       child: Column(
                         children: [
-                          ListTile(
-                            leading: CircleAvatar(
-                              backgroundImage: NetworkImage(
-                                  article?[index].urlToImage ?? 'no data'),
+                          Stack(children: [
+                            Padding(
+                              padding: const EdgeInsets.all(6.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(12.0)),
+                                child: Image(
+                                  image: NetworkImage(
+                                      article?[index].urlToImage ?? ''),
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
                             ),
-                            title: Text(article?[index].author ?? 'no data'),
-                            trailing: Text(
-                              article?[index].publishedAt ?? 'no data',
+                            Positioned(
+                                bottom: 20.0,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(12.0),
+                                  child: SizedBox(
+                                    width: 300,
+                                    child: Text(
+                                      article?[index].title ?? 'no data',
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w800,
+                                          backgroundColor:
+                                              Color.fromARGB(90, 0, 0, 0),
+                                          fontSize: 18.0),
+                                    ),
+                                  ),
+                                ))
+                          ]),
+                          ListTile(
+                            title: Text(
+                              article?[index].author ?? 'no data',
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                            subtitle: Text(
+                              finalTime,
                             ),
                           ),
                           Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Text(article?[index].title ?? 'no data'),
+                            child: Text(
+                              article?[index].title ?? 'no data',
+                              style: const TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w700),
+                            ),
                           ),
                           Padding(
                               padding: const EdgeInsets.all(8.0),
